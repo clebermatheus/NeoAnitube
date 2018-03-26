5da5a4d2af18523407cd58614b816d521358201f
Cleber Matheus <clebermatheus@outlook.com>
2018-03-25 22:42:49 -0300

    Adicionado Objeto Episódio

---
+++ app/src/main/java/com/github/clebermatheus/neoanitube/animes/model/Episodio.kt
@@ -0,0 +1,17 @@
+package com.github.clebermatheus.neoanitube.animes.model
+
+/**
+ * Objeto Episódio
+ *
+ * Created by clebermatheus on 25/03/18.
+ */
+
+data class Episodio(
+    val vid: Int,
+    val title: String,
+    val numero: String,
+    val imagem: String,
+    val _0: Int,
+    val _1: String,
+    val _2: String
+)
