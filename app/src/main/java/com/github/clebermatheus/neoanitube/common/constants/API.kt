414d225a0f96fafc7dcc05dfea273a864e496edb
Cleber Matheus <clebermatheus@outlook.com>
2018-03-25 23:46:28 -0300

    Adicionado classe API

---
+++ app/src/main/java/com/github/clebermatheus/neoanitube/common/constants/API.kt
@@ -0,0 +1,12 @@
+package com.github.clebermatheus.neoanitube.common.constants
+
+/**
+ * Endereços da AnitubeBR API
+ *
+ * Created by clebermatheus on 25/03/18.
+ */
+object API {
+    const val URL = "http://anitubebr.biz/api/"
+    const val LANCAMENTOS = this.URL+"lancamentos.php"
+    const val ULTIMOS = this.URL+"ultimos.php"
+}
