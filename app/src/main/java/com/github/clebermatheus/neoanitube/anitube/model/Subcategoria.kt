a567aaf6e79a08ed8fb5f08058407a989e2b2cbc
Cleber Matheus <clebermatheus@outlook.com>
2018-04-01 02:14:44 -0300

    Implementado a aba Animes

---
+++ app/src/main/java/com/github/clebermatheus/neoanitube/anitube/model/Subcategoria.kt
@@ -0,0 +1,11 @@
+package com.github.clebermatheus.neoanitube.anitube.model
+
+/**
+ *
+ * Implementação do objeto subcategoria
+ *
+ * Created by clebermatheus on 01/04/18.
+ */
+data class Subcategoria(
+    val SUBCATEGORIAS: ArrayList<Anime>
+)
---
+++ app/src/main/java/com/github/clebermatheus/neoanitube/anitube/viewmodels/AnimesViewAdapter.kt
@@ -0,0 +1,56 @@
+package com.github.clebermatheus.neoanitube.anitube.viewmodels
+
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.github.clebermatheus.neoanitube.R
+import com.github.clebermatheus.neoanitube.anitube.model.Anime
+import com.github.clebermatheus.neoanitube.anitube.model.Episodio
+
+/**
+ * Adapter do Recycler View da LancamentosFragment
+ *
+ * Created by clebermatheus on 27/03/18.
+ */
+class AnimesViewAdapter(private val animes: ArrayList<Anime>) :
+        RecyclerView.Adapter<AnimesViewAdapter.ViewHolder>() {
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val text: TextView = v.findViewById(R.id.titulo)
+
+        init {
+            v.setOnClickListener({
+                Log.d(TAG_DEBUG, "Element $adapterPosition clicked.")
+            })
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val adapterView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.adapter_lancamentos, parent, false) as View
+        return ViewHolder(adapterView)
+    }
+
+    override fun getItemCount(): Int = animes.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.text = animes[position].name
+    }
+
+    fun add(anime: Anime){
+        animes.add(anime)
+        notifyDataSetChanged()
+    }
+
+    fun remove(anime: Anime) {
+        animes.remove(anime)
+        notifyDataSetChanged()
+    }
+
+    companion object {
+        private val TAG_DEBUG = "LancamentosViewAdapter"
+    }
+}
