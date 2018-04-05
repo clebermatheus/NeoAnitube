26f9857fdad0fd8d7df22f18be8afd61d8df40a6
Cleber Matheus <clebermatheus@outlook.com>
2018-04-05 01:31:36 -0300

    API alterado

--- app/src/main/java/com/github/clebermatheus/neoanitube/anitube/constants/API.kt
+++ app/src/main/java/com/github/clebermatheus/neoanitube/anitube/constants/API.kt
@@ -6,11 +6,13 @@ package com.github.clebermatheus.neoanitube.anitube.constants
  * Created by clebermatheus on 25/03/18.
  */
 object API {
-    const val URL = "http://anitubebr.biz/api/"
+    private const val URL = "http://anitubebr.biz/api/"
+    private const val URL_CDN = "https://cdn.anitube.info/"
+    const val URL_EPISODIO = "http://fr40.anitube.info/"
     const val LANCAMENTOS = URL +"lancamentos.php"
     const val SUBCATEGORIA = URL +"subcategorias/"
     const val ULTIMOS = URL +"ultimos.php"
 
-    const val CAPA = "https://cdn.anitube.info/media/categories/video/"
-    const val CAPA_EPISODIOS = "https://cdn.anitube.info/media/videos/tmb/"
+    const val CAPA = URL_CDN+"media/categories/video/"
+    const val CAPA_EPISODIOS = URL_CDN+"media/videos/tmb/"
 }
