a346ba78cfb1c8228faf7e3fd406aaa4474a00f0
Cleber Matheus <clebermatheus@outlook.com>
2018-05-20 01:12:28 -0300

    Atualizado endereços da API (2/2)

---
+++ app/src/main/java/com/github/clebermatheus/neoanitube/anitube/views/adapter/AnimesViewAdapter.kt
@@ -0,0 +1,96 @@
+package com.github.clebermatheus.neoanitube.anitube.views.adapter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.facebook.drawee.view.SimpleDraweeView
+import com.github.clebermatheus.neoanitube.R
+import com.github.clebermatheus.neoanitube.anitube.constants.API
+import com.github.clebermatheus.neoanitube.anitube.model.Anime
+import com.github.clebermatheus.neoanitube.anitube.views.BottomMenuAnimeFragment
+import com.google.gson.Gson
+
+/**
+ * Adapter para a exibição da lista de animes
+ *
+ * Created by clebermatheus on 27/03/18.
+ */
+class AnimesViewAdapter(
+    private val animes: ArrayList<Anime>,
+    private val activity: AppCompatActivity,
+    private val tipoView: Boolean = false
+) : RecyclerView.Adapter<AnimesViewAdapter.ViewHolder>() {
+
+    class ViewHolder(
+        v: View,
+        activity: AppCompatActivity
+    ) : RecyclerView.ViewHolder(v) {
+        val capa: SimpleDraweeView = v.findViewById(R.id.capa)
+        val text: TextView = v.findViewById(R.id.titulo)
+        lateinit var anime: Anime
+
+        init {
+            v.setOnClickListener {
+                Log.d(TAG_DEBUG, "Element $adapterPosition clicked.")
+                val dialogFragment = BottomMenuAnimeFragment()
+                val args = Bundle()
+                args.putString("anime", Gson().toJson(anime))
+                dialogFragment.arguments = args
+                dialogFragment.show(activity.supportFragmentManager, dialogFragment.tag)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val adapterView = if(tipoView) {
+            layoutInflater.inflate(R.layout.adapter_grid, parent, false)
+        } else {
+            layoutInflater.inflate(R.layout.adapter_list, parent, false)
+        }
+        return ViewHolder(adapterView, activity)
+    }
+
+    override fun getItemCount(): Int = animes.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.text = animes[position].name
+        holder.capa.setImageURI(API.CAPA+animes[position].capa)
+        holder.anime = animes[position]
+    }
+
+    fun add(anime: Anime): AnimesViewAdapter {
+        animes.add(anime)
+        notifyDataSetChanged()
+
+        return this
+    }
+
+    fun addAll(animes: List<Anime>): AnimesViewAdapter {
+        animes.forEach { this.animes.add(it) }
+        Log.i(TAG_DEBUG, animes.toString())
+        notifyDataSetChanged()
+
+        return this
+    }
+
+    fun clear(): AnimesViewAdapter {
+        animes.clear()
+        notifyDataSetChanged()
+
+        return this
+    }
+
+    operator fun component1(): ArrayList<Anime> = this.animes
+    operator fun component2(): AppCompatActivity = this.activity
+    operator fun component3(): Boolean = this.tipoView
+
+    companion object {
+        private const val TAG_DEBUG = "AnimesViewAdapter"
+    }
+}
